425c5984b384c40a3e06d1e73cb647513f027f69
fada21 <lukasz.czaplicki@gmail.com>
2015-04-12 15:05:15 +0100

    initial

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "diCaptainPlanet"
+
+version := "1.0"
+
+scalaVersion := "2.11.6"
+
+libraryDependencies += "com.softwaremill.macwire" %% "macros" % "1.0.1"
+
+libraryDependencies += "com.softwaremill.macwire" %% "runtime" % "1.0.1"
---
+++ src/main/scala/MacWirePlayground.scala
@@ -0,0 +1,92 @@
+import com.softwaremill.macwire._
+
+object MacWirePlayground extends App {
+
+  trait LogCreation {
+    println(s"... assembling $this")
+  }
+
+  abstract class EnergySource extends LogCreation
+
+  case class OilWell() extends EnergySource
+
+  case class CoalMine() extends EnergySource
+
+  case class EvilWorkers() extends LogCreation
+
+  case class WastePlant(energySource: EnergySource, evilWorkers: EvilWorkers) extends LogCreation {
+    def pollute(): Pollution = {
+      val pollution = energySource match {
+        case OilWell() => Pollution("Oil spills")
+        case CoalMine() => Pollution("Smog")
+      }
+      println(s"${pollution.pollutionType} polluted Mother Earth.")
+      pollution
+    }
+  }
+
+  case class Pollution(pollutionType: String) extends LogCreation
+
+  case class Earth() extends LogCreation
+
+  case class Fire() extends LogCreation
+
+  case class Wind() extends LogCreation
+
+  case class Water() extends LogCreation
+
+  case class Heart() extends LogCreation
+
+  case class CaptainPlanet(earth: Earth, fire: Fire, wind: Wind, water: Water, hearth: Heart) extends LogCreation {
+    println("By your powers combined.. I am Captain Planet!")
+
+    def eliminate(pollution: Pollution) = {
+      println(s"${pollution.pollutionType} eliminated. Go Planet!")
+    }
+  }
+
+  trait EnergySourceOilModule {
+    lazy val energySource = wire[OilWell]
+  }
+
+  trait EnergySourceCoalModule {
+    lazy val energySource = wire[CoalMine]
+  }
+
+  trait PollutionModule {
+    lazy val wastePlant = wire[WastePlant]
+
+    lazy val evilWorkers = wire[EvilWorkers]
+
+    def energySource: EnergySource
+  }
+
+  trait PlaneteersModule {
+    lazy val captainPlanet = wire[CaptainPlanet]
+
+    lazy val earth = wire[Earth]
+    lazy val fire = wire[Fire]
+    lazy val wind = wire[Wind]
+    lazy val water = wire[Water]
+    lazy val heart = wire[Heart]
+  }
+
+  trait AppModule extends PlaneteersModule with PollutionModule {
+
+    def go = {
+      println()
+      val pollution = wastePlant.pollute()
+      captainPlanet.eliminate(pollution)
+
+    }
+  }
+
+  val oilModule = new AppModule with EnergySourceOilModule
+
+  val coalModule = new AppModule with EnergySourceCoalModule
+
+  oilModule.go
+  oilModule.go
+  coalModule.go
+
+}
